57bdc083f5304e02eec30157b9ab961894d18b25
Dorhmi Tarik <62470307+TarikDorhmi01@users.noreply.github.com>
2020-11-26 18:23:35 +0100

    Navbar add

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import Navbar from './Components/Navbar'
 
 function App() {
   return (
     <div className="App">
-      
+        <Navbar>
+
+        </Navbar>
     </div>
+      
   );
 }
 
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,10 +1,44 @@
 import React from 'react'
 import './Navbar.css'
+import { 
+        Link,
+        BrowserRouter as Router,
+        Route,
+        Switch
+       } from 'react-router-dom'
+
 
 function Navbar() {
     return (
         <div className="Navbar">
-            
+           <Router>
+           <nav>
+                <ul>
+                    <li>
+                    <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                    <Link to="/Skills">Skills</Link>
+                    </li>
+                    <li>
+                    <Link to="/Projects">Projects</Link>
+                    </li>
+                </ul>
+            </nav>
+            <Switch>
+                <Route path="/">
+                    { /* Insertion of home component is going to be over here   */ }
+                </Route>
+                <Route path="/Skills">
+                    { /* Insertion of Skills component is going to be over here   */ }
+                </Route>
+                <Route path="/Projects">
+                    { /* Insertion of Projects component is going to be over here   */ }
+                </Route>
+            </Switch>
+           </Router>
+           
+          
         </div>
     )
 }
